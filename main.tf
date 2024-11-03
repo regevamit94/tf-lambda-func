b53107dc786c2cf83c39bcd82b4be07d3353f4b1
regevamit94 <ubuntu@ip-172-26-12-16.eu-central-1.compute.internal>
2024-11-03 18:02:10 +0000

    http api gateway added

--- main.tf
+++ main.tf
@@ -12,17 +12,6 @@ provider "aws" {
 }
 
 
-
-resource "aws_eip" "my-eip" {
-  domain = "vpc"
-  
-  tags = {
-    Name = "amit-eip"
-  }
-}
-
-
-
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -30,8 +19,10 @@ module "vpc" {
   cidr = "192.168.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b"]
-  public_subnets = ["192.168.0.0/17", "192.168.128.0/17"]
-  
+  public_subnets = ["192.168.0.0/17"]
+  private_subnets = ["192.168.128.0/17"]
+
+  enable_nat_gateway = true
   map_public_ip_on_launch = true  
 
   tags = {
@@ -55,6 +46,13 @@ resource "aws_security_group" "my_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -84,7 +82,7 @@ resource "aws_efs_file_system" "my-efs" {
 
 resource "aws_efs_mount_target" "efs-location" {
   file_system_id = aws_efs_file_system.my-efs.id
-  subnet_id      = module.vpc.public_subnets[0]
+  subnet_id      = module.vpc.private_subnets[0]
 }
 
 
@@ -127,23 +125,114 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
+
+resource "aws_iam_role_policy" "lambda_to_vpc_policy" {
+  name   = "lambda-vpc-policy"
+  role   = aws_iam_role.iam_for_lambda.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:CreateNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DeleteNetworkInterface"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_efs_access_point" "access_point_for_lambda" {
+  file_system_id = aws_efs_file_system.my-efs.id
+  root_directory {
+    path = "/mnt/efs"
+  }
+
+  tags = {
+    Name = "amit-access-point"
+  }
+}
+
 resource "aws_lambda_function" "my_lambda_func" {
   function_name = "amit-lambda-function"
   filename      = "download_from_url_lambda_file.zip"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "download_from_url_lambda_file.lambda_handler"
-
   runtime = "python3.9"
+
+  file_system_config {
+    arn = aws_efs_access_point.access_point_for_lambda.arn
+    local_mount_path = "/mnt/efs"
+  }
+
+  vpc_config {
+    subnet_ids         = module.vpc.private_subnets
+    security_group_ids = [aws_security_group.my_sg.id]
+  }
+
 
   environment {
     variables = {
       FILE_URL = var.url
     }
   }
-
+  depends_on = [aws_efs_mount_target.efs-location]
 }
 
-resource "aws_eip_association" "eip-assignment" {
-  instance_id = aws_instance.my-ec2-inst.id
-  allocation_id = aws_eip.my-eip.id
+resource "aws_apigatewayv2_api" "my_http_api" {
+  name          = "amit-http-api"
+  protocol_type = "HTTP"
+
+  tags = {
+    Name = "amit-http-api"
+  }
+}
+
+# Create a Lambda Integration for the HTTP API
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id             = aws_apigatewayv2_api.my_http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.my_lambda_func.arn
+  integration_method = "POST"
+
+  depends_on = [aws_lambda_function.my_lambda_func]
+}
+
+# Create a route for the HTTP API
+resource "aws_apigatewayv2_route" "my_http_api_route" {
+  api_id    = aws_apigatewayv2_api.my_http_api.id
+  route_key = "POST /myendpoint" # Define the endpoint path
+
+  target = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+# Deploy the HTTP API
+resource "aws_apigatewayv2_stage" "my_http_stage" {
+  api_id = aws_apigatewayv2_api.my_http_api.id
+  name   = "prod" 
+
+  depends_on = [aws_apigatewayv2_route.my_http_api_route]
+}
+
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.my_lambda_func.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.my_http_api.execution_arn}/*/*"
 }
